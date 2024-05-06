948c75f9fb2290f1b8ccea650ac87a14ca1c7959
Tássyla Lima <tassyla.lima@taqtile.com.br>
2024-05-06 13:33:39 -0300

    :sparkles: GraphQL for an API server over HTTP

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,39 @@
-var { graphql, buildSchema } = require("graphql")
- 
+var express = require("express");
+var { createHandler } = require("graphql-http/lib/use/express");
+var { buildSchema } = require("graphql");
+var { ruruHTML } = require("ruru/server");
+
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
   type Query {
     hello: String
   }
-`)
- 
-// The rootValue provides a resolver function for each API endpoint
-var rootValue = {
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
   hello() {
-    return "Hello world!"
-  }
-}
- 
-// Run the GraphQL query '{ hello }' and print out the response
-graphql({
-  schema,
-  source: "{ hello }",
-  rootValue
-}).then(response => {
-  console.log(response)
-})
+    return "Hello world!";
+  },
+};
+
+var app = express();
+
+// Create and use the GraphQL handler.
+app.all(
+  "/graphql",
+  createHandler({
+    schema: schema,
+    rootValue: root,
+  })
+);
+
+// Serve the GraphiQL IDE.
+app.get("/", (_req, res) => {
+  res.type("html");
+  res.end(ruruHTML({ endpoint: "/graphql" }));
+});
+
+// Start the server at port
+app.listen(4000);
+console.log("Running a GraphQL API server at http://localhost:4000/graphql");
