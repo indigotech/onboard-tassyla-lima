96c8ebeec09d3aa2f02e4da600b433a52ed74dee
Tássyla Lima <tassyla.lima@taqtile.com.br>
2024-05-15 09:49:43 -0300

    :construction: progress on saving user in database

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,14 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import typeDefs from './schema/typeDefs.js';
 import resolvers from './schema/resolvers.js';
+import setupDatabase from './setup-database.js';
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
+
+await setupDatabase();
 
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
--- src/schema/resolvers.ts
+++ src/schema/resolvers.ts
@@ -1,17 +1,31 @@
+import { AppDataSource } from '../data-source.js';
+import { User } from '../entity/User.js';
+
 const resolvers = {
   Query: {
     hello: () => 'Hello, World!',
   },
   Mutation: {
-    createUser: (_, { data }): User => {
-      const id = 1;
-      return { id, ...data };
+    createUser: async (_, { data }): Promise<OutUser> => {
+      console.log('Inserting a new user into the database...');
+      const user = new User();
+      user.name = data.name;
+      user.email = data.email;
+      user.birthDate = data.birthDate;
+      user.password = data.password;
+      await AppDataSource.manager.save(user);
+      console.log('Saved a new user with id: ' + user.id);
+
+      const storedUser = await AppDataSource.manager.findOneBy(User, {
+        id: user.id,
+      });
+      return storedUser;
     },
   },
 };
 
-interface User {
-  id: string;
+interface OutUser {
+  id: number;
   name: string;
   email: string;
   birthDate: string;
