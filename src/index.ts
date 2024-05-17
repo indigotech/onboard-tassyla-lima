c23aaec2c16795fc436e5baaf9918ed6588a4779
Tássyla Lima <tassyla.lima@taqtile.com.br>
2024-05-17 09:44:19 -0300

    :bug: fixes setups

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import { setupDatabase } from './setup-database.js';
 import { setupServer } from './setup-server.js';
 
-before(async () => {
-  await setupDatabase();
-  await setupServer();
-});
+await setupDatabase();
+await setupServer();
