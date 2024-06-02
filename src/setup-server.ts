0caffd6843a80f1779a37b716a6f3113db01dd79
Tássyla Lima <tassyla.lima@taqtile.com.br>
2024-06-02 18:14:59 -0300

    :sparkles: authorizes access

--- src/setup-server.ts
+++ src/setup-server.ts
@@ -1,8 +1,9 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import { errorFormatter } from './schema/customError.js';
 import typeDefs from './schema/typeDefs.js';
 import resolvers from './schema/resolvers.js';
-import { errorFormatter } from './schema/customError.js';
+import jwt from 'jsonwebtoken';
 
 export let serverUrl: string;
 
@@ -15,6 +16,22 @@ export async function setupServer() {
 
   const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
+    context: async ({ req }) => {
+      const token = req.headers.authorization || '';
+      if (token) {
+        const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET) as {
+          id: number;
+          iat: number;
+          exp: number;
+        };
+
+        if (new Date().getTime() < decodedToken.exp * 1000) {
+          return { userId: decodedToken.id };
+        }
+      }
+
+      return {};
+    },
   });
 
   console.log(`🚀  Server ready at: ${url}`);
